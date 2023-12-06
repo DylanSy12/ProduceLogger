8bcc4f0931b21ec3d93d88ef21b8f1ca10c8d889
DylanSy12 <dylanmsy12@gmail.com>
2023-12-06 11:30:14 -0800

    created input screen

--- app/src/main/java/com/example/producelogger/HarvestLog.kt
+++ app/src/main/java/com/example/producelogger/HarvestLog.kt
@@ -3,7 +3,10 @@ package com.example.producelogger
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,16 +14,21 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.semantics.Role.Companion.Button
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -36,55 +44,97 @@ fun HarvestLogComposable(navController: NavController) {
         color = MaterialTheme.colorScheme.background
     ) {
         // A surface container using the 'background' color from the theme
-        HarvestedProduce(produceList = harvestList)
+        HarvestedProduce(navController = navController, produceList = harvestList)
     }
 }
 @Composable
-fun HarvestedProduce(produceList: ArrayList<Map<String, String>>) {
-    Row(
-        horizontalArrangement = Arrangement.Center,
-        modifier = Modifier
-            .padding(0.dp, 20.dp)
-            .fillMaxWidth()
-    ) {
-        Text(
-            "Date Harvested", modifier = Modifier
-                .size(180.dp, 40.dp)
-                .padding(10.dp, 0.dp),
-            style = TextStyle(
-                fontWeight = FontWeight.Bold,
-                fontSize = 25.sp
-            )
-        )
-
-        Text(
-            "Item", modifier = Modifier
-                .size(360.dp, 40.dp)
-                .padding(10.dp, 0.dp),
-            style = TextStyle(
-                fontWeight = FontWeight.Bold,
-                fontSize = 25.sp
-            )
-        )
-        Text(
-            "Weight (lbs.)", modifier = Modifier
-                .size(180.dp, 40.dp)
-                .padding(10.dp, 0.dp),
-            style = TextStyle(
-                fontWeight = FontWeight.Bold,
-                fontSize = 25.sp
-            )
-        )
-    }
-
-    LazyColumn(horizontalAlignment = Alignment.CenterHorizontally) {
-        items(produceList) { harvest ->
-            if (harvest["date"] != null && harvest["item"] != null && harvest["weight"] != null) {
-                AddItem(
-                    dateHarvested = harvest["date"]!!,
-                    produce = harvest["item"]!!,
-                    weight = harvest["weight"]!!
+fun HarvestedProduce(navController: NavController, produceList: ArrayList<Map<String, String>>) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(color = Color.Green),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Produce Logger",
+                style = TextStyle(
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 50.sp,
+                    textAlign = TextAlign.Center
                 )
+            )
+        }
+        Divider()
+        Button(
+            modifier = Modifier.padding(15.dp),
+            onClick = {
+                navController.navigate(Screen.HarvestRecorder.route) {
+                    popUpTo(Screen.HarvestRecorder.route) {
+                        saveState = true
+                        inclusive = true
+                    }
+                }
+            }
+        ) {
+            Text(
+                text = "Record Harvest",
+                style = TextStyle(
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 30.sp,
+                    textAlign = TextAlign.Center
+                )
+            )
+        }
+        Divider()
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .padding(0.dp, 20.dp)
+                .fillMaxWidth()
+        ) {
+            Text(
+                "Date", modifier = Modifier
+                    .size(180.dp, 40.dp)
+                    .padding(10.dp, 0.dp),
+                style = TextStyle(
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 30.sp,
+                    textAlign = TextAlign.Center
+                )
+            )
+
+            Text(
+                "Item", modifier = Modifier
+                    .size(360.dp, 40.dp)
+                    .padding(10.dp, 0.dp),
+                style = TextStyle(
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 30.sp,
+                    textAlign = TextAlign.Center
+                )
+            )
+            Text(
+                "Weight", modifier = Modifier
+                    .size(180.dp, 40.dp)
+                    .padding(10.dp, 0.dp),
+                style = TextStyle(
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 30.sp,
+                    textAlign = TextAlign.Center
+                )
+            )
+        }
+        Divider()
+        LazyColumn(horizontalAlignment = Alignment.CenterHorizontally) {
+            items(produceList) { harvest ->
+                if (harvest["date"] != null && harvest["item"] != null && harvest["weight"] != null) {
+                    AddItem(
+                        dateHarvested = harvest["date"]!!,
+                        produce = harvest["item"]!!,
+                        weight = harvest["weight"]!!
+                    )
+                }
             }
         }
     }
@@ -102,7 +152,8 @@ fun AddItem(dateHarvested: String, produce: String, weight: String) {
                 .size(180.dp, 40.dp)
                 .padding(10.dp, 5.dp),
             style = TextStyle(
-                fontSize = 10.sp
+                fontSize = 25.sp,
+                textAlign = TextAlign.Center
             )
         )
         Text(
@@ -111,7 +162,8 @@ fun AddItem(dateHarvested: String, produce: String, weight: String) {
                 .size(360.dp, 40.dp)
                 .padding(10.dp, 5.dp),
             style = TextStyle(
-                fontSize = 10.sp
+                fontSize = 25.sp,
+                textAlign = TextAlign.Center
             )
         )
         Text(
@@ -120,7 +172,8 @@ fun AddItem(dateHarvested: String, produce: String, weight: String) {
                 .size(180.dp, 40.dp)
                 .padding(10.dp, 5.dp),
             style = TextStyle(
-                fontSize = 10.sp
+                fontSize = 25.sp,
+                textAlign = TextAlign.Center
             )
         )
     }
--- app/src/main/java/com/example/producelogger/HarvestRecorder.kt
+++ app/src/main/java/com/example/producelogger/HarvestRecorder.kt
@@ -1,19 +1,131 @@
 package com.example.producelogger
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.producelogger.ui.theme.ProduceLoggerTheme
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
 
+fun Date.toString(format: String, locale: Locale = Locale.getDefault()): String {
+    val formatter = SimpleDateFormat(format, locale)
+    return formatter.format(this)
+}
+
+fun getCurrentDateTime(): Date {
+    return Calendar.getInstance().time
+}
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HarvestRecorderComposable(navController: NavController) {
+    var harvest: MutableMap<String, String> = mutableMapOf()
     Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(
+                text = "Date",
+                style = TextStyle(
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 30.sp,
+                    textAlign = TextAlign.Center
+                )
+            )
+            var temp = getCurrentDateTime().toString("MM/dd/yyyy")
+            var date by remember { mutableStateOf(TextFieldValue(temp)) }
+            harvest["date"] = temp
+            TextField(
+                value = date,
+                onValueChange = {
+                    date = it
+                    harvest["date"] = date.text
+                },
+                textStyle = TextStyle(
+                    fontSize = 25.sp,
+                    textAlign = TextAlign.Center
+                )
+            )
+            Text(
+                text = "Item",
+                style = TextStyle(
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 30.sp,
+                    textAlign = TextAlign.Center
+                )
+            )
+            var item by remember { mutableStateOf(TextFieldValue("")) }
+            TextField(
+                value = item,
+                onValueChange = {
+                    item = it
+                    harvest["item"] = item.text
+                },
+                textStyle = TextStyle(
+                    fontSize = 25.sp,
+                    textAlign = TextAlign.Center
+                )
+            )
+            Text(
+                text = "Weight (lbs.)",
+                style = TextStyle(
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 30.sp,
+                    textAlign = TextAlign.Center
+                )
+            )
+            var weight by remember { mutableStateOf(TextFieldValue("")) }
+            TextField(
+                value = weight,
+                onValueChange = {
+                    weight = it
+                    harvest["weight"] = weight.text+" lbs."
+                },
+                textStyle = TextStyle(
+                    fontSize = 25.sp,
+                    textAlign = TextAlign.Center
+                )
+            )
+            Button(
+                onClick = {
+                    harvestList.add(harvest)
+                    navController.navigate(Screen.HarvestLog.route) {
+                        popUpTo(Screen.HarvestLog.route) {
+                            saveState = true
+                            inclusive = true
+                        }
+                    }
+                }
+            ) {
+                Text(
+                    text = "Record Harvest",
+                    style = TextStyle(
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 30.sp,
+                        textAlign = TextAlign.Center
+                    )
+                )
+            }
+        }
     }
 }
 
